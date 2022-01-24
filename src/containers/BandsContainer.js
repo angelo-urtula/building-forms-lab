bbdaa20e7b762f13e61dbc93b5877b04161a445b
angelo <aurtula1@gmail.com>
2022-01-24 05:11:03 -0500

    commit tree c71d8e66299ebe0d36168e9a8631671b8bcce3de

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
 
 class BandsContainer extends Component {
+
+  renderBands = () => this.props.bands.map((band, id) => <li>{band.id}.{band.name}</li>)
   render() {
     return(
       <div>
+        <ol>
+       {this.renderBands()}
+       </ol>
         BandsContainer
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+
+export default connect(mapStateToProps)(BandsContainer)
